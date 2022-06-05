0efce57c0c4fca8f9c270723393bda40d4408ee8
Maycon Jardim <mayconsmill@gmail.com>
2022-06-05 14:05:35 -0300

    componente chamado list  criado e estilizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -43,6 +43,7 @@ import { ClienteCreateComponent } from './components/cliente/cliente-create/clie
 import { ClienteListComponent } from './components/cliente/cliente-list/cliente-list.component';
 import { ClienteUpdateComponent } from './components/cliente/cliente-update/cliente-update.component';
 import { ClienteDeleteComponent } from './components/cliente/cliente-delete/cliente-delete.component';
+import { ChamadoListComponent } from './components/chamado/chamado-list/chamado-list.component';
 
 
 
@@ -60,7 +61,8 @@ import { ClienteDeleteComponent } from './components/cliente/cliente-delete/clie
     ClienteListComponent,
     ClienteCreateComponent,
     ClienteUpdateComponent,
-    ClienteDeleteComponent
+    ClienteDeleteComponent,
+    ChamadoListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/chamado/chamado-list/chamado-list.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Chamado } from 'src/app/models/chamado';
+
+@Component({
+  selector: 'app-chamado-list',
+  templateUrl: './chamado-list.component.html',
+  styleUrls: ['./chamado-list.component.css']
+})
+export class ChamadoListComponent implements OnInit {
+
+  ELEMENT_DATA: Chamado[] = [
+    {
+      id: 1,
+      dataAbertura: '21/03/2018',
+      dataFechamento: '25/03/2022',
+      prioridade: 'ALTA',
+      status: 'ANDAMENTO',
+      titulo: 'Chamado 01',
+      descricao: 'Teste chamado 1',
+      tecnico: 1,
+      cliente: 6,
+      nomeCliente: 'Fabio Ronaldo',
+      nomeTecnico: 'Maycon Jardim'
+    }
+  ]
+  
+
+  displayedColumns: string[] = ['id', 'titulo', 'cliente', 'tecnico', 'dataAbertura', 'prioridade', 'status', 'acoes'];
+  dataSource = new MatTableDataSource<Chamado>(this.ELEMENT_DATA);
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+}
