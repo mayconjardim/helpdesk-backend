b09807390339c4c0563518f65c7403e7b0bf826b
Maycon Jardim <mayconsmill@gmail.com>
2022-06-05 11:18:38 -0300

    botoes de acoes para tecnico

--- src/app/services/tecnico.service.ts
+++ src/app/services/tecnico.service.ts
@@ -15,4 +15,8 @@ export class TecnicoService {
     return this.http.get<Tecnico[]>(`${API_CONFIG.baseUrl}/tecnicos`);
   }
 
+  create(tecnico: Tecnico): Observable<Tecnico> {
+    return this.http.post<Tecnico>(`${API_CONFIG.baseUrl}/tecnicos`, tecnico);
+  }
+
 }
